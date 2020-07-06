ea75f0044d161e418a25a94153a8071929dd1c6c
Ryan Gurnick <ryangurnick@gmail.com>
2020-07-05 21:57:00 -0700

    adding in some good main.go code

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"TrackerJacker/core/cross"
+	"TrackerJacker/core/enc"
+	"TrackerJacker/core/parsing"
+	"fmt"
+	"io/ioutil"
+)
+
+const encKey = "Password"
+const payload = "payload.txt"
+
+func generatePayload(inFile string) {
+	data, _ := ioutil.ReadFile(inFile)
+	enc.EncryptFile(payload, data, encKey)
+}
+
+func parsePayload() parsing.PayloadType {
+	str := enc.DecryptFile(payload, encKey)
+
+	var payload parsing.PayloadType
+	parsing.ParsePayload(str, &payload)
+	return payload
+}
+
+func main() {
+	generatePayload("input.json")
+	payload := parsePayload()
+	for i := 0; i < len(payload); i++ {
+		if payload[i].Namespace == "files" {
+			out := cross.FileParse(payload[i].Arguments, payload[i].Result)
+			fmt.Printf("Namespace %s | Command %s | Output: %t\n", payload[i].Namespace, payload[i].Arguments, out)
+		} else if payload[i].Namespace == "hosts" {
+			out := cross.HostParse(payload[i].Arguments, payload[i].Result)
+			fmt.Printf("Namespace %s | Command %s | Output: %t\n", payload[i].Namespace, payload[i].Arguments, out)
+		} else {
+			fmt.Printf("Unrecognized Namespace: %s\n", payload[i].Namespace)
+		}
+	}
+}
