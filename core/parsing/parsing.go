2556e08872ca8fe8c4a36fc40296f1bac91bd6e4
Ryan Gurnick <ryangurnick@gmail.com>
2020-07-05 21:43:46 -0700

    add overall parser for json

--- core/parsing/parsing.go
+++ core/parsing/parsing.go
@@ -1,2 +1,14 @@
 package parsing
 
+import "encoding/json"
+
+type PayloadType []struct {
+	Namespace string   `json:"namespace"`
+	Arguments []string `json:"arguments"`
+	Result    interface{}     `json:"result"`
+}
+
+func ParsePayload(str []byte, payload *PayloadType) *PayloadType {
+	json.Unmarshal([]byte(str), &payload)
+	return payload
+}
