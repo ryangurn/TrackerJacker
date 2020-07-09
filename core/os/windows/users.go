c9f501db52fd245fee4b4f15e81505b8d51a92e6
Ryan Gurnick <ryangurnick@gmail.com>
2020-07-09 12:24:07 -0700

    Adding in MetaCheck

--- core/os/windows/users.go
+++ core/os/windows/users.go
@@ -5,7 +5,7 @@ import (
 	wapi "github.com/iamacarpet/go-win64api"
 )
 
-func WAPIUserExist(usr string) (retBool bool) {
+func UserExist(usr string) (retBool bool) {
 	retBool = false
 
 	users, err := wapi.ListLocalUsers()
@@ -23,6 +23,74 @@ func WAPIUserExist(usr string) (retBool bool) {
 	return
 }
 
+func UserMeta(usr string, key string, value interface{}) (retBool bool) {
+	retBool = false
+
+	users, err := wapi.ListLocalUsers()
+	if err != nil {
+		fmt.Printf("Error fetching user list, %s\r\n", err.Error())
+		return
+	}
+
+	for _, u := range users {
+		if usr == u.Username {
+			if key == "BadPasswordCount" {
+				if value == u.BadPasswordCount {
+					retBool = true
+					return
+				}
+			} else if key == "FullName" {
+				if value == u.FullName {
+					retBool = true
+					return
+				}
+			} else if key == "IsAdmin" {
+				if value == u.IsAdmin {
+					retBool = true
+					return
+				}
+			} else if key == "IsEnabled" {
+				if value == u.IsEnabled {
+					retBool = true
+					return
+				}
+			} else if key == "IsLocked" {
+				if value == u.IsLocked {
+					retBool = true
+					return
+				}
+			} else if key == "LastLogin" {
+				if value == u.LastLogon {
+					retBool = true
+					return
+				}
+			} else if key == "NoChangePassword" {
+				if value == u.NoChangePassword {
+					retBool = true
+					return
+				}
+			} else if key == "NumberOfLogons" {
+				if value == u.NumberOfLogons {
+					retBool = true
+					return
+				}
+			} else if key == "PasswordAge" {
+				if value == u.PasswordAge {
+					retBool = true
+					return
+				}
+			} else if key == "PasswordNeverExpires" {
+				if value == u.PasswordNeverExpires {
+					retBool = true
+					return
+				}
+			}
+		}
+	}
+
+	return
+}
+
 func UserParse(args []string, result interface{}) (retBool bool) {
 	retBool = false
 
@@ -31,7 +99,11 @@ func UserParse(args []string, result interface{}) (retBool bool) {
 	}
 
 	if args[0] == "exist" {
-		if WAPIUserExist(args[1]) == result {
+		if UserExist(args[1]) == result {
+			retBool = true
+		}
+	} else if args[0] == "meta" {
+		if UserMeta(args[1], "IsAdmin", true) == result {
 			retBool = true
 		}
 	} else {
