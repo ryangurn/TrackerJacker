9f45c4920e1ca9a6ede7920d0ad75c877bdc40f5
Ryan Gurnick <ryangurnick@gmail.com>
2020-08-01 20:26:51 -0700

    Adding Additional Meta Tests

--- core/os/windows/softwares_test.go
+++ core/os/windows/softwares_test.go
@@ -50,6 +50,20 @@ func TestSoftwareMeta(t *testing.T) {
 		{ name: "Software Meta Valid Args - Publisher False",  args: args{software: "Microsoft Edge", key: "Publisher", value: "Inc"}, wantRetBool: false},
 		{ name: "Software Meta Valid Args - InstallDate True",  args: args{software: "Microsoft Edge", key: "InstallDate", value: testingTime}, wantRetBool: true},
 		{ name: "Software Meta Valid Args - InstallDate False",  args: args{software: "Microsoft Edge", key: "InstallDate", value: time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)}, wantRetBool: false},
+		{ name: "Software Meta Valid Args - EstimatedSize True", args: args{software: "Microsoft Edge", key: "EstimatedSize", value: "0"}, wantRetBool: true},
+		{ name: "Software Meta Valid Args - EstimatedSize False", args: args{software: "Microsoft Edge", key: "EstimatedSize", value: "-1"}, wantRetBool: false},
+		{ name: "Software Meta Valid Args - Contact True", args: args{software: "Microsoft Edge", key: "Contact", value: ""}, wantRetBool: true},
+		{ name: "Software Meta Valid Args - Contact False", args: args{software: "Microsoft Edge", key: "Contact", value: "-1"}, wantRetBool: false},
+		{ name: "Software Meta Valid Args - HelpLink True", args: args{software: "Microsoft Edge", key: "HelpLink", value: ""}, wantRetBool: true},
+		{ name: "Software Meta Valid Args - HelpLink False", args: args{software: "Microsoft Edge", key: "HelpLink", value: "-1"}, wantRetBool: false},
+		{ name: "Software Meta Valid Args - InstallSource True", args: args{software: "Microsoft Edge", key: "InstallSource", value: ""}, wantRetBool: true},
+		{ name: "Software Meta Valid Args - InstallSource False", args: args{software: "Microsoft Edge", key: "InstallSource", value: "-1"}, wantRetBool: false},
+		{ name: "Software Meta Valid Args - InstallLocation True", args: args{software: "Microsoft Edge", key: "InstallLocation", value: "C:\\Program Files (x86)\\Microsoft\\Edge\\Application"}, wantRetBool: true},
+		{ name: "Software Meta Valid Args - InstallLocation False", args: args{software: "Microsoft Edge", key: "InstallLocation", value: "-1"}, wantRetBool: false},
+		{ name: "Software Meta Valid Args - VersionMajor True", args: args{software: "Microsoft Edge", key: "VersionMajor", value: "522"}, wantRetBool: true},
+		{ name: "Software Meta Valid Args - VersionMajor False", args: args{software: "Microsoft Edge", key: "VersionMajor", value: "-1"}, wantRetBool: false},
+		{ name: "Software Meta Valid Args - VersionMinor True", args: args{software: "Microsoft Edge", key: "VersionMinor", value: "52"}, wantRetBool: true},
+		{ name: "Software Meta Valid Args - VersionMinor False", args: args{software: "Microsoft Edge", key: "VersionMinor", value: "-1"}, wantRetBool: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
